2169acb5901ae2fe43d897a0a4d5676da4fe9b61
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-03-16 13:11:26 -0500

    Add * restrictions apply to Same-Day Processing

--- sites/debt-collection/main.tsx
+++ sites/debt-collection/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Debt Collection Mailing Service - Generated from template-microsite
- * Generated at: 2026-03-16T18:09:25.902Z
+ * Generated at: 2026-03-16T18:11:10.655Z
  */
 
 import { createRoot } from 'react-dom/client';
