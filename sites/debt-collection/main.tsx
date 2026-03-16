c9132a6ba8432629e23ef08e5718a18479ffa292
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-03-16 14:16:34 -0500

    Update FAQs for debt collection

--- sites/debt-collection/main.tsx
+++ sites/debt-collection/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Debt Collection Mailing Service - Generated from template-microsite
- * Generated at: 2026-03-16T18:44:33.785Z
+ * Generated at: 2026-03-16T19:16:27.468Z
  */
 
 import { createRoot } from 'react-dom/client';
