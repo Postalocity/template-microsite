d6c2dac01a2cbafa146369342cd6cc97b963f971
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-03-16 13:18:53 -0500

    Fix footer - read from content.footer in JSON

--- sites/debt-collection/main.tsx
+++ sites/debt-collection/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Debt Collection Mailing Service - Generated from template-microsite
- * Generated at: 2026-03-16T18:11:10.655Z
+ * Generated at: 2026-03-16T18:18:45.532Z
  */
 
 import { createRoot } from 'react-dom/client';
