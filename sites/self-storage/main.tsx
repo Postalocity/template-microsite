d17ded0b7e60fae58c42e2803648aaff3e50e287
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-03-17 08:40:43 -0500

    Rebuild self-storage with navigation fix

--- sites/self-storage/main.tsx
+++ sites/self-storage/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Self Storage Mailing Service - Generated from template-microsite
- * Generated at: 2026-03-17T13:38:34.389Z
+ * Generated at: 2026-03-17T13:40:36.929Z
  */
 
 import { createRoot } from 'react-dom/client';
