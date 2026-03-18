38980e7a136c257f7709ec235bb8f99029a7670d
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-03-18 11:56:37 -0500

    chore: Rebuild debt-collection site with updated copy

--- sites/postalocity/debt-collection/main.tsx
+++ sites/postalocity/debt-collection/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Debt Collection Mailing Service - Generated from template-microsite
- * Generated at: 2026-03-18T16:50:36.633Z
+ * Generated at: 2026-03-18T16:56:31.203Z
  * Brand: Postalocity
  */
 
