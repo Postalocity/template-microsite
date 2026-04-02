1a4fe7ea1dc32229dd4c7056a80e8095870f23b7
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-04-02 16:14:15 -0500

    chore(promo): rebuild with updated 'Fulfill' workflow step

--- sites/broadstroke/promo/main.tsx
+++ sites/broadstroke/promo/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Promotional Products & Apparel — Broadstroke, Inc. - Generated from template-microsite
- * Generated at: 2026-04-02T21:11:10.394Z
+ * Generated at: 2026-04-02T21:14:08.902Z
  * Brand: Broadstroke
  */
 
