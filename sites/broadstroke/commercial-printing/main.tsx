0675572d66455d1caeef6e2a878ef9824ae3492d
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-04-02 11:29:23 -0500

    chore(commercial-printing): regenerate site with simplified title

--- sites/broadstroke/commercial-printing/main.tsx
+++ sites/broadstroke/commercial-printing/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Commercial Printing — Broadstroke, Inc. - Generated from template-microsite
- * Generated at: 2026-04-02T16:17:49.422Z
+ * Generated at: 2026-04-02T16:29:16.811Z
  * Brand: Broadstroke
  */
 
