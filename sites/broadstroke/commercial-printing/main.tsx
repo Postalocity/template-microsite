0c9f34c77ef924382a954b3564dc5673cb3feb25
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-04-02 15:41:11 -0500

    chore(commercial-printing): regenerate with Technology link pointing to aobts.com

--- sites/broadstroke/commercial-printing/main.tsx
+++ sites/broadstroke/commercial-printing/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Commercial Printing — Broadstroke, Inc. - Generated from template-microsite
- * Generated at: 2026-04-02T16:58:13.258Z
+ * Generated at: 2026-04-02T20:41:01.362Z
  * Brand: Broadstroke
  */
 
