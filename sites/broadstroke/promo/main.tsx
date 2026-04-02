d9c05fd190501c5d3998325b20ee24e8adda142d
Henry Tafolla <henrytafolla@Mac.broadstrokeinc.lan>
2026-04-02 12:38:03 -0500

    chore(promo): regenerate site with fixed comparison table icons

--- sites/broadstroke/promo/main.tsx
+++ sites/broadstroke/promo/main.tsx
@@ -1,6 +1,6 @@
 /**
  * Promotional Products & Apparel — Broadstroke, Inc. - Generated from template-microsite
- * Generated at: 2026-04-02T17:26:33.148Z
+ * Generated at: 2026-04-02T17:37:56.327Z
  * Brand: Broadstroke
  */
 
